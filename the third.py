8bdcca3fecc0cb7a02d6d3ca7a5c51417326173f
Thisissmkasian <45357127+Thisissmkasian@users.noreply.github.com>
2023-11-11 19:18:13 +0200

    the third.py

--- the third.py
+++ the third.py
@@ -1,13 +1,17 @@
-def drukuy_litery_B(symbol):
-    # Виводимо літеру "В" у вигляді 5 рядків
-    for i in range(5):
-        if i == 0 or i == 2 or i == 4:
-            print(symbol * 2 + " " + symbol * 2)
-        else:
-            print(symbol + " " * 2 + symbol)
+"""
+Задача №3
+Напишіть програму для друку літери В висотою 5 рядків за 
+допомогою введеного користувачем символу.
 
-# Запитуємо користувача ввести символ для відображення літери "В"
-korystuvach_symbol = input("Введіть символ: ")
 
-# Викликаємо функцію та передаємо користувацький символ
-drukuy_litery_B(korystuvach_symbol)
+
+Автор: КАСЬЯН М.А.
+
+"""
+symbol = input("Введіть символ для відображення літери 'В': ")
+
+print(symbol + " " + symbol)
+print(symbol + " " + symbol)
+print(symbol + symbol + symbol)
+print(symbol + " " + symbol)
+print(symbol + " " + symbol)
